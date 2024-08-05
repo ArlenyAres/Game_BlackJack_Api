9663934de08cfdf15ca929928d3eff25cbcce769
Arleny <arleny.ares@gmail.com>
2024-08-05 11:11:51 +0200

    create GlobalExceptionHandler

--- blackjack/src/main/java/com/GameBlackjack/blackjack/model/Game.java
+++ blackjack/src/main/java/com/GameBlackjack/blackjack/model/Game.java
@@ -1,6 +1,7 @@
 package com.GameBlackjack.blackjack.model;
 
 
+import com.GameBlackjack.blackjack.exception.ResourceNotFoundException;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -16,5 +17,84 @@ public class Game {
     private Dealer dealer;
     private boolean isActive;
     private int currentPlayerIndex;
+
+    public void starGame(List<Player> players) {
+        this.players = players;
+        this.deck = new Deck();
+       // this.deck.initializeDeck();
+        this.deck.shuffle();
+        this.dealer = new Dealer();
+        this.dealer.dealCard(deck);
+        this.isActive = true;
+        this.currentPlayerIndex = 0;
+
+        for (Player player : players) {
+            player.setHands(List.of(deck.draw(), deck.draw()));
+            player.setPlaying(true);
+        }
+
+    }
+
+    public Player getCurrentPlayer() {
+        return players.get(currentPlayerIndex);
+    }
+
+
+    public void nextTurn() {
+
+        currentPlayerIndex += 1;
+        if (currentPlayerIndex >= players.size()) {
+            isActive = false;
+            dealer.playTurn(deck);
+        }
+    }
+
+    public void playerHit(){
+
+        Player currentPlayer = getCurrentPlayer();
+        currentPlayer.getHands().add(deck.draw());
+
+        if(getHandValue(currentPlayer.getHands()) > 21){
+            currentPlayer.setPlaying(false);
+        }
+    }
+
+//    public void playerStand(){
+//        Player currentPlayer = getCurrentPlayer();
+//        currentPlayer.setPlaying(false);
+//    }
+
+    public void playerStand(){
+        getCurrentPlayer().setPlaying(false);
+        nextTurn();
+    }
+
+    public int getHandValue(List<Card> hand) {
+        int totalValue = 0;
+        int numAces = 0;
+
+        for (Card card : hand) {
+            totalValue += card.getValue();
+            if (card.getRank().equals("A")) {
+                numAces++;
+            }
+        }
+        while (totalValue > 21 && numAces > 0) {
+            totalValue -= 10;
+            numAces--;
+        }
+        return totalValue;
+    }
+
+    public void placeBet(Long playerId, int amount) {
+
+        Player player = players.stream()
+                .filter(p -> p.getId() == playerId)
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Player not found with id: " + playerId));
+
+    }
+
+
 
 }
